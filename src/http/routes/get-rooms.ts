fad97c55e98c0795acd388adb33545384a86f464
Rafael Valverde <rafaverde@msn.com>
2025-07-09 11:29:44 -0300

    feat: implement room and question management routes, including creation and retrieval

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -1,6 +1,8 @@
 import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
 import { db } from "../../db/connection.ts";
 import { schema } from "../../db/schema/index.ts";
+import { count, eq } from "drizzle-orm";
+import { questions } from "../../db/schema/questions.ts";
 
 export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
   app.get("/rooms", async () => {
@@ -8,8 +10,12 @@ export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
       .select({
         id: schema.rooms.id,
         name: schema.rooms.name,
+        createdAt: schema.rooms.createdAt,
+        questionCounts: count(questions.id),
       })
       .from(schema.rooms)
+      .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
+      .groupBy(schema.rooms.id)
       .orderBy(schema.rooms.createdAt);
 
     return results;
